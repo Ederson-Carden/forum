4951725acb56d9682e0ee4191fdbe3b95d990c67
carden <lonersads@163.com>
2023-06-08 17:16:01 +0800

    “基础框架创建”

---
+++ src/components/home.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>
+        <van-sticky>
+            <van-nav-bar class="bar_sty" left-text="论坛">
+                <template #right>
+                    <van-icon name="search" size="20" />
+                </template>
+            </van-nav-bar>
+            <div class="title_menu">
+                <van-button class="smallBtn" size="small">推荐</van-button>
+                <van-button class="smallBtn" size="small">最新</van-button>
+                <van-button class="filterBtn" size="small">筛选</van-button>
+            </div>
+        </van-sticky>
+        <!-- 帖子 -->
+        <div>
+            <index_List v-for="n in post" :itemPost=n></index_List>
+        </div>
+
+        <!-- 标签栏 -->
+        <van-tabbar v-model="active">
+            <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+            <van-tabbar-item icon="plus">发布</van-tabbar-item>
+            <van-tabbar-item icon="user-o">鄙人</van-tabbar-item>
+        </van-tabbar>
+
+    </div>
+</template>
+
+<script>
+import index_List from './module/index_List'
+
+export default {
+    name: 'home',
+    data() {
+        return {
+            active: 0,
+            post: [{
+                head: '',
+                name: "临时抱佛脚",
+                date: "2022/05/06",
+                content: {
+                    text: '树大招风',
+                    img: ''
+                },
+            },
+            ]
+        }
+    },
+    components: { index_List },
+}
+</script>
+
+<style scoped>
+.bar_sty {
+    background: #14cbb1;
+}
+
+.van-nav-bar_text {
+    font-size: .35rem;
+    color: #fff;
+}
+
+.smallBtn {
+    border: 0;
+    margin: .1rem 0 0 0;
+}
+
+.filterBtn {
+    float: right;
+    border: 0;
+    margin: .1rem .1rem 0 0;
+    background: #f2f3f5;
+}
+
+.title_menu {
+    background: #fff;
+    height: 3rem;
+}
+
+</style>
---
+++ src/components/module/index_List.vue
@@ -0,0 +1,146 @@
+<template>
+    <div style="text-align: left;">
+            <div class="container-content">
+                <div class="user-info">
+                    <img class="user-head-image"
+                        src="https://tse3-mm.cn.bing.net/th/id/OIP-C.fEdSZduT7aHQ8CxR22bnFwHaFA?w=258&h=180&c=7&r=0&o=5&pid=1.7"
+                        alt="">
+                    <div class="user-name-time">
+                        <div class="user-name">{{itemPost.name}}</div>
+                        <div class="user-publish-time ">{{itemPost.date}}</div>
+                    </div>
+                </div>
+                <div class="content" >
+                    <div class='content-text'>{{itemPost.content.text}}</div>
+                    <img class='content-img'
+                        src="https://tse3-mm.cn.bing.net/th/id/OIP-C.fEdSZduT7aHQ8CxR22bnFwHaFA?w=258&h=180&c=7&r=0&o=5&pid=1.7">
+                </div>
+            </div>
+            <div class="container-line"></div>
+        </div>
+</template>
+<script>
+export default {
+    name: 'index_List',
+    data() {
+        return {
+            
+        }
+    },
+    props:['itemPost']
+}
+</script>
+<style scoped>
+
+.user-name {
+    font-size: 18px;
+    color: #333;
+    font-weight: bold;
+}
+
+.content-style {
+    width: 36px;
+    height: 36px;
+    line-height: 36px;
+    border-radius: 25%;
+    background-color: #ccc;
+}
+
+.user-publish-time {
+    font-size: 15px;
+    color: #999;
+}
+
+.container-line {
+    width: 100%;
+    height: 1px;
+    background-color: #ccc;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+}
+
+.container-content {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 16px 16px;
+    margin: 0 auto;
+}
+
+.user-info {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+}
+
+.user-head-image {
+    width: 36px;
+    height: 36px;
+    line-height: 36px;
+    border-radius: 50%;
+    background-color: #ccc;
+    margin-top: 10px;
+}
+
+.user-name-time {
+    display: flex;
+    flex-direction: column;
+    width: 68%;
+    margin-left: 10px;
+    margin-top: 5px;
+}
+
+.user-name {
+    font-size: 18px;
+    color: #333;
+    font-weight: bold;
+}
+
+.content-style {
+    width: 36px;
+    height: 36px;
+    line-height: 36px;
+    border-radius: 25%;
+    background-color: #ccc;
+}
+
+.user-publish-time {
+    font-size: 15px;
+    color: #999;
+}
+
+.content {
+    display: flex;
+    flex-direction: column;
+    margin-top: 14px;
+}
+
+.content-scroll {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    position: relative;
+    z-index: -1;
+}
+
+.content-text {
+    margin-left: 10px;
+    font-size: 18px;
+    color: #666;
+}
+
+.content-img {
+    width: 100px;
+    height: 100px;
+    margin-left: 10px;
+    margin-top: 10px;
+    background-color: #999;
+
+}
+</style>
