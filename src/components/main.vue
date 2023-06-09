d8950e287478f101d741cf5602754328c0d04009
张京鹏 <9091087+zjpplus@user.noreply.gitee.com>
2023-06-09 22:26:14 +0800

    “搜索实现”

--- src/components/main.vue
+++ src/components/main.vue
@@ -3,13 +3,17 @@
         <van-sticky>
             <van-nav-bar class="bar_sty" left-text="论坛">
                 <template #right>
-                    <van-icon name="search" size="20" />
+                    <van-icon name="search" is-link @click="showPopup" size="20" />
+                    <van-popup v-model:show="show" position="top" :style="{ padding: '0px' }">
+                        <van-search v-model="searchContent" shape="round" background="#4fc08d" placeholder="请输入搜索关键词" @search="searchHandler"/>
+                    </van-popup>
                 </template>
             </van-nav-bar>
             <div class="title_menu">
                 <!-- <van-button class="smallBtn" size="small">推荐</van-button> -->
                 <van-button class="smallBtn" size="small">最新</van-button>
-                <van-popover placement="right-start" v-model:show="showPopover" :actions="actions" actions-direction="horizontal">
+                <van-popover placement="right-start" v-model:show="showPopover" :actions="actions"
+                    actions-direction="horizontal">
                     <template #reference>
                         <van-button class="smallBtn" size="small">筛选</van-button>
                     </template>
@@ -33,29 +37,60 @@ export default {
     name: 'main',
     data() {
         return {
-            post: []
+            post: [],
+            searchContent:"",
         }
     },
     components: { index_List },
     created() {
-        this.$store.state.post = JSON.parse(localStorage.getItem("postInfo")) ?JSON.parse(localStorage.getItem("postInfo"))  : this.$store.state.post
+        this.$store.state.post = JSON.parse(localStorage.getItem("postInfo")) ? JSON.parse(localStorage.getItem("postInfo")) : this.$store.state.post
         this.post = this.$store.state.post
         console.log(this.$store.state.post)
     },
     setup() {
-    const showPopover = ref(false);
-    const actions = [
-      { text: '闲聊' },
-      { text: '表白墙' },
-      { text: '寻物启事' },
-      { text: '校内新闻' },
-    ];
+        const showPopover = ref(false);
+        const actions = [
+            { text: '闲聊' },
+            { text: '表白墙' },
+            { text: '寻物启事' },
+            { text: '校内新闻' },
+        ];
+        const value=ref('');
+        const show = ref(false);
+        const showPopup = () => {
+            show.value = true;
+        };
+        return {
+            value,
+            show,
+            showPopup,
+            actions,
+            showPopover,
 
-    return {
-      actions,
-      showPopover,
-    };
-  },
+        };
+    },
+     methods: {
+    searchHandler() {
+        let search = this.searchContent.toLowerCase();
+		      let newList = [];
+		      if (search) {
+		    //     this.post.filter(item => {
+		    //       if (
+		    //         item.text.toLowerCase().indexOf(search) !== -1 
+            //         // || item.age.toLowerCase().indexOf(_keyWord) !== -1
+		    //       ) {
+		    //         newList.push(item);
+		    //       }
+		    //     });
+                newList = this.post.filter( item => item.text.includes(search))
+                this.post = newList;
+		      }
+              else {
+                this.post=this.$store.state.post
+              }
+		      
+		    },
+        }
 }
 </script>
 
