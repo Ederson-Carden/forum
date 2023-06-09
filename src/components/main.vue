10ab651f1f3c50c3ecd7665c436b27919d2970c8
carden <lonersads@163.com>
2023-06-10 01:58:27 +0800

    时间排序

--- src/components/main.vue
+++ src/components/main.vue
@@ -5,13 +5,21 @@
                 <template #right>
                     <van-icon name="search" is-link @click="showPopup" size="20" />
                     <van-popup v-model:show="show" position="top" :style="{ padding: '0px' }">
-                        <van-search v-model="searchContent" shape="round" background="#4fc08d" placeholder="请输入搜索关键词" @search="searchHandler"/>
+                        <van-search v-model="searchContent" shape="round" background="#4fc08d" placeholder="请输入搜索关键词"
+                            @search="searchHandler" />
                     </van-popup>
                 </template>
             </van-nav-bar>
             <div class="title_menu">
                 <!-- <van-button class="smallBtn" size="small">推荐</van-button> -->
-                <van-button class="smallBtn" size="small">最新</van-button>
+                <van-button class="smallBtn" size="small" @click="sortbyid">
+                <div v-if="newflag">
+                    最早
+                </div>
+                <div v-else>
+                    最新
+                </div>
+                </van-button>
                 <van-popover placement="right-start" v-model:show="showPopover" :actions="actions"
                     actions-direction="horizontal">
                     <template #reference>
@@ -38,13 +46,14 @@ export default {
     data() {
         return {
             post: [],
-            searchContent:"",
+            searchContent: "",
+            newflag: false,
         }
     },
     components: { index_List },
     created() {
         this.$store.state.post = JSON.parse(localStorage.getItem("postInfo")) ? JSON.parse(localStorage.getItem("postInfo")) : this.$store.state.post
-        this.$store.state.NewContentID = localStorage.getItem("NewContentID") ?localStorage.getItem("NewContentID"): this.$store.state.NewContentID
+        this.$store.state.NewContentID = localStorage.getItem("NewContentID") ? localStorage.getItem("NewContentID") : this.$store.state.NewContentID
         this.post = this.$store.state.post
         // const neww = this.post.filter(p => p.text.includes('树'))
         // console.log(neww)
@@ -59,7 +68,7 @@ export default {
             { text: '寻物启事' },
             { text: '校内新闻' },
         ];
-        const value=ref('');
+        const value = ref('');
         const show = ref(false);
         const showPopup = () => {
             show.value = true;
@@ -75,29 +84,44 @@ export default {
     },
     methods: {
         GotoDetail(value) {
-            this.$router.push({name:'detail',params: {id:value}})
+            this.$router.push({ name: 'detail', params: { id: value } })
         },
-    searchHandler() {
-        let search = this.searchContent.toLowerCase();
-		      let newList = [];
-		      if (search) {
-		    //     this.post.filter(item => {
-		    //       if (
-		    //         item.text.toLowerCase().indexOf(search) !== -1 
-            //         // || item.age.toLowerCase().indexOf(_keyWord) !== -1
-		    //       ) {
-		    //         newList.push(item);
-		    //       }
-		    //     });
-                newList = this.post.filter( item => item.text.includes(search))
+        sortbyid() {
+            if (this.newflag == false) {
+                let newList = [];
+                newList = this.post.sort((a, b) => a.id < b.id ? 1 : a.id > b.id ? -1 : 0);
+                this.sort=newList
+                console.log(this.post)
+                this.newflag = true
+            }else
+            {   this.$store.state.post = JSON.parse(localStorage.getItem("postInfo"))
+                this.post = this.$store.state.post
+                console.log(this.$store.state.post)
+                console.log(this.post)
+                this.newflag = false
+            }
+        },
+        searchHandler() {
+            let search = this.searchContent.toLowerCase();
+            let newList = [];
+            if (search) {
+                //     this.post.filter(item => {
+                //       if (
+                //         item.text.toLowerCase().indexOf(search) !== -1 
+                //         // || item.age.toLowerCase().indexOf(_keyWord) !== -1
+                //       ) {
+                //         newList.push(item);
+                //       }
+                //     });
+                newList = this.post.filter(item => item.text.includes(search))
                 this.post = newList;
-		      }
-              else {
-                this.post=this.$store.state.post
-              }
-		      
-		    },
-        }
+            }
+            else {
+                this.post = this.$store.state.post
+            }
+
+        },
+    }
 }
 </script>
 
