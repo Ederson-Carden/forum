5dfb931851a654059bd4f87aef7217fb95e380fe
张京鹏 <9091087+zjpplus@user.noreply.gitee.com>
2023-06-09 13:29:23 +0800

    footer

--- src/components/module/bottom.vue
+++ src/components/module/bottom.vue
@@ -1,8 +1,8 @@
 <template>
         <van-tabbar v-model="active">
-            <van-tabbar-item name="main" icon="home-o" to="main">首页</van-tabbar-item>
+            <van-tabbar-item name="main" icon="home-o" to="/home/main">首页</van-tabbar-item>
             <van-tabbar-item name="" icon="plus">发布</van-tabbar-item>
-            <van-tabbar-item name="me" icon="user-o" to="me">鄙人</van-tabbar-item>
+            <van-tabbar-item name="me" icon="user-o" to="/home/me">鄙人</van-tabbar-item>
         </van-tabbar>
 </template>
 <script>
