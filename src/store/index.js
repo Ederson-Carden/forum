67430c523d40fc9a1d68130d93bd148f7cf7b1f5
carden <lonersads@163.com>
2023-06-09 12:56:57 +0800

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,11 @@
 import { createStore } from 'vuex'
 
-
-
 export default createStore({
   state: {
     name:"张三",
     age:12,
-    count:0
+    count: 0,
+    userInfo:''
   },
   getters: {
   },
